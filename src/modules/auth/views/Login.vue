1dd6d9aafede4042cb7fd8780150ce02ee9c1412
Lucas <lucas_fernandomendes@hotmail.com>
2020-04-20 19:27:48 -0300

    merge

---
+++ src/modules/auth/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+	<mdb-container>
+		<h1>Login</h1>
+	</mdb-container>
+</template>
+
+<script>
+import { mdbContainer } from "mdbvue";
+
+export default {
+	components: {
+		mdbContainer
+	}
+};
+</script>
