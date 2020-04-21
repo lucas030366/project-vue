532e5be3acd33ad1bc1eaca35412da25818da8a2
lucas030366 <lucas_fernandomendes@hotmail.com>
2020-04-21 18:47:23 -0300

    apollo

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import '@fortawesome/fontawesome-free/css/all.min.css'
-import '@/plugins/vuelidate'
-import 'bootstrap-css-only/css/bootstrap.min.css'
-import 'mdbootstrap/css/mdb.min.css'
-
 import Vue from 'vue'
 import App from '@/App'
 import router from '@/router'
 import store from '@/store'
+
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import '@/plugins/vuelidate'
+import 'bootstrap-css-only/css/bootstrap.min.css'
+import 'mdbootstrap/css/mdb.min.css'
 
 Vue.config.productionTip = false
 
--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,35 +1,35 @@
 <template>
 	<mdb-container>
-
 		<mdb-col lg="4" class="mx-auto mt-5">
-
 			<form>
-
-				<mdb-card class="z-depth-5">
-					<mdb-card-header color="teal" class="text-center">LOGIN</mdb-card-header>
+				<mdb-card class="z-depth-5 elegant-color">
+					<mdb-card-header color="teal" class="text-center">{{ texts.text }}</mdb-card-header>
 
 					<mdb-card-body>
-						<mdb-input label="Email" type="email" icon="envelope" v-model.trim="$v.user.email.$model" />
-						<mdb-input label="Senha" type="password" icon="lock" v-model.trim="$v.user.password.$model" />
-						<mdb-btn @click="log" class="text-center">Criar Conta</mdb-btn>
+						<mdb-input v-if="!isLogin" label="Nome" type="text" icon="user-circle white-text" v-model.trim="$v.user.name.$model"/>
+
+						<mdb-input label="Email" type="email" icon="envelope white-text" v-model.trim="$v.user.email.$model" />
+
+						<mdb-input label="Senha" type="password" icon="lock white-text" v-model.trim="$v.user.password.$model" />
 					</mdb-card-body>
+	
+					<h6 @click="isLogin = !isLogin" class="white-text font-weight-bolder text-center">{{ texts.button }}</h6>
 
-					<mdb-footer>
-						<mdb-btn :disabled="$v.$invalid" @click="submit" color="teal" size="md" class="float-right">Login</mdb-btn>
-					</mdb-footer>
+					<button type="submit" :disabled="$v.$invalid" @click="submit" class="btn btn-md teal float-right white-text">
+						{{ texts.text }}
+					</button>
+
 				</mdb-card>
-
 			</form>
-
 		</mdb-col>
-
 	</mdb-container>
 </template>
 
 <script>
 import * as mdbvue from "mdbvue";
 
-import { required, email, minLength } from "vuelidate/lib/validators"
+import { required, email, minLength } from "vuelidate/lib/validators";
+
 export default {
 	name: "Login",
 	components: {
@@ -37,30 +37,39 @@ export default {
 	},
 	data() {
 		return {
+			user: { name: null, email: null, password: null },
+			isLogin: true
+		};
+	},
+	validations() {
+		const validations = {
 			user: {
-				email: null,
-				password: null
+				email: { required, email },
+				password: { required, minLength: minLength(6) }
+			}
+		};
+
+		if (this.isLogin) {
+			return validations;
+		}
+
+		return {
+			user: {
+				...validations.user,
+				name: { required, minLength: minLength(3) }
 			}
 		};
 	},
-	validations: {
-		user:{
-			email: {
-				required, 
-				email
-			},
-			password: {
-				required,
-				minLength: minLength(6)
-			}
+	computed: {
+		texts() {
+			return this.isLogin
+				? { text: "Login", button: "Criar Conta" }
+				: { text: "Criar Conta", button: "Já tenho uma conta" };
 		}
 	},
 	methods: {
-		log(){
-			console.log("Vuelidate: ", this.$v)
-		},
-		submit(){
-			console.log("User: ", this.user)
+		submit() {
+			console.log("User: ", this.user);
 		}
 	}
 };
