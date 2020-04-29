dd48194d1e4d64806d1186ec1e4c8c00c21e252e
lucas030366 <lucas_fernandomendes@hotmail.com>
2020-04-28 21:46:56 -0300

    tela de login

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -1,3 +1,36 @@
 <template>
-	<h3>Login</h3>
+	<v-container fill-height color="red">
+		<v-col lg="4" class="mx-auto">
+
+			<v-card class="elevation-5">
+				<v-toolbar color="primary" dark>
+					<v-toolbar-title>Login</v-toolbar-title>
+				</v-toolbar>
+
+				<v-card-text>
+					<v-form>
+						<v-text-field prepend-icon="fas fa-envelope" label="email" name="email" type="email">
+						</v-text-field>
+						<v-text-field prepend-icon="fas fa-lock" label="senha" name="password" type="password"></v-text-field>
+					</v-form>
+
+					<v-btn block depressed>Criar conta</v-btn>
+
+				</v-card-text>
+
+				<v-card-actions>
+					<v-spacer/>
+					<v-btn color="primary" large>Login</v-btn>
+				</v-card-actions>
+
+			</v-card>
+
+		</v-col>
+	</v-container>
 </template>
+
+<script>
+export default {
+	name: "Login"
+};
+</script>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import pt from 'vuetify/es5/locale/pt';
+import "@fortawesome/fontawesome-free/css/all.css"
 
 Vue.use(Vuetify);
 
