f6c05b071c072dbd18da026157d2694e3f6232a7
lucas030366 <lucas_fernandomendes@hotmail.com>
2020-05-04 21:57:50 -0300

    exibindo lancamentos

---
+++ src/modules/dashboard/modules/finances/components/RecordsList.vue
@@ -0,0 +1,32 @@
+<template>
+	<div>
+		<v-list v-if="records.length > 0" two-line>    
+			<RecordsListItem v-for="record in records" :key="record.id" :record="record" />
+		</v-list>
+
+   <v-alert v-else type="warning" dense bolder="top" class="mt-4">
+      Sem registros
+    </v-alert>
+	</div>
+</template>
+
+<script>
+import RecordsListItem from "./RecordsListItem";
+
+import RecordsService from "../services/records-service";
+
+export default {
+	name: "RecordsList",
+	components: {
+		RecordsListItem
+	},
+	data() {
+		return {
+			records: {}
+		};
+	},
+	async created() {
+    this.records = await RecordsService.records();
+	}
+};
+</script>
