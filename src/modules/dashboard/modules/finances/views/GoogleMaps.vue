9c26477b0f9bae507379f29196374099a42e33af
lucas030366 <lucas_fernandomendes@hotmail.com>
2020-06-28 20:21:51 -0300

    google-maps

--- src/modules/dashboard/modules/finances/views/GoogleMaps.vue
+++ src/modules/dashboard/modules/finances/views/GoogleMaps.vue
@@ -1,13 +1,28 @@
 <template>
 	<v-container>
+		<p>{{ center}}</p>
 		<googlemaps-map
+			v-if="this.map.lat"
 			:center="center"
 			:zoom="17"
 			:options="{styles: styles['hide']}"
+			:streetViewControl="false"
 			style="height:500px"
 		>
-			<googlemaps-marker v-for="marker of markers" :key="marker._id" :position="marker.position" />
-		</googlemaps-map>
+			<googlemaps-marker
+				v-for="loja of lojas"
+				:key="loja._id"
+				:position="loja.position"
+				:icon="loja.icon"
+				@click="infoWindow(loja)"
+			/>
+		</googlemaps-map>		
+
+		<v-sheet v-else color="grey " class="px-3 pt-3 pb-3">
+			<v-skeleton-loader class="mx-auto" max-width="300" type="card"></v-skeleton-loader>
+		</v-sheet>
+
+		<h3>Teste</h3>
 	</v-container>
 </template>
 
@@ -16,12 +31,52 @@ export default {
 	name: "GoogleMaps",
 	data() {
 		return {
-			center: { lat: -25.3482577, lng: -49.1792121 },
-			markers: [
-				{ position: { lat: -25.3482577, lng: -49.1792121 } },
-				{ position: { lat: -25.3499388, lng: -49.1779297 } }
-			],
-			styles: {
+			map: { lat: null, lng: null },
+			linkIcone: "https://via.placeholder.com/50"
+		};
+	},
+	methods: {
+		local() {
+			if (localStorage.center) {
+				this.map = JSON.parse(localStorage.center);
+			} else {
+				navigator.geolocation.getCurrentPosition(
+					position => {
+						(this.map.lat = position.coords.latitude),
+							(this.map.lng = position.coords.longitude);
+					},
+					error => {
+						console.log(error.message);
+					}
+				);
+			}
+		},
+		infoWindow(loja) {
+			console.log("InfoView:", loja.info);
+		}
+	},
+	computed: {
+		center() {
+			return { lat: this.map.lat, lng: this.map.lng };
+		},
+		lojas() {
+			return [
+				{
+					position: { lat: this.map.lat, lng: this.map.lng }, //, icon: this.linkIcone
+					info: "Meu Local"
+				},
+				{
+					position: { lat: -25.3499388, lng: -49.1779297 },
+					info: "teste1"
+				},
+				{
+					position: { lat: -25.3484276, lng: -49.178759 },
+					info: "teste2"
+				}
+			];
+		},
+		styles() {
+			return {
 				hide: [
 					{
 						featureType: "all",
@@ -32,8 +87,11 @@ export default {
 						stylers: [{ visibility: "on" }]
 					}
 				]
-			}
-		};
+			};
+		}
+	},
+	created() {
+		this.local();
 	}
 };
 </script>
